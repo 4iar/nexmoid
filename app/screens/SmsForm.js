fd367031bcd4c3de4fca450d7c8b4ce6935f2632
4iar <me@4iar.com>
2017-07-09 19:40:27 +0100

    Implement SmsForm

---
+++ app/screens/SmsForm.js
@@ -0,0 +1,63 @@
+import React, { Component } from 'react';
+import { View } from 'react-native';
+import { RkButton, RkTextInput, RkStyleSheet, RkText } from 'react-native-ui-kitten';
+import { scaleVertical } from '../utils/scale';
+
+
+export default class SmSForm extends Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      to: "",
+      from: "",
+      text: ""
+    }
+  }
+
+  handleSubmit () {
+    this.props.handleSubmit({
+      from: this.state.from,
+      to: this.state.to,
+      text: this.state.text
+    });
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <RkText>SMS Composer</RkText>
+
+        <RkTextInput label="To"
+                     onChangeText={text => this.setState({to: text})}
+        />
+
+        <RkTextInput label="From"
+                     onChangeText={text => this.setState({from: text})}
+        />
+
+        <RkTextInput label="Text"
+                     onChangeText={text => this.setState({text: text})}
+        />
+
+        <View style={styles.buttons}>
+          <RkButton onPress={this.handleSubmit.bind(this)} label="Send" rkType="success">
+            <RkText>Send</RkText>
+          </RkButton>
+        </View>
+      </View>
+    )
+  }
+}
+
+let styles = RkStyleSheet.create(theme => ({
+  container: {
+    paddingHorizontal: 17,
+    paddingBottom: scaleVertical(22),
+    alignItems: 'center',
+    flex: -1
+  },
+  buttons: {
+    flexDirection: 'row',
+    marginBottom: scaleVertical(24)
+  }
+}));
